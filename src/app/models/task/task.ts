b071c5205e28e3b4029dd1ad325b6964cf376362
Ali Mosavi <alirezamosavi346@gmail.com>
2019-02-25 19:15:21 +0330

    Add date to Task

--- src/app/models/task/task.ts
+++ src/app/models/task/task.ts
@@ -1,9 +1,11 @@
 export class Task {
   name: string;
   done: boolean;
+  date: Date;
 
-  constructor(name: string, done: boolean = false) {
+  constructor(name: string, done?: boolean, date?: Date) {
     this.name = name;
-    this.done = done;
+    this.done = done ? done : false;
+    this.date = date ? date : new Date();
   }
 }
