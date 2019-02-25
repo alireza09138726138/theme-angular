63250ed98eb7ded3ee6a359459645c83da2da420
Ali Mosavi <alirezamosavi346@gmail.com>
2019-02-26 01:13:55 +0330

    Add Group list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Task } from './models/task/task';
+import {Component} from '@angular/core';
+import {Task} from './models/task/task';
+import {Group} from './models/group/group';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +15,45 @@ export class AppComponent {
   tasks: Array<Task> = [];
 
   /**
+   * List of groups
+   */
+  groups: Array<Group> = [
+    new Group('Hello', [
+      new Task('Buy milk', false, null, false),
+      new Task('Buy coke', false, null, false),
+      new Task('Buy shisha', false, null, false),
+    ]),
+    new Group('Hello', [
+      new Task('Drink milk', false, null, false),
+      new Task('Drink coke', false, null, false),
+      new Task('Smoke shisha', false, null, false),
+    ]),
+  ];
+
+  /**
    * Task to add from input
    */
   taskToAdd: string;
 
+  /**
+   * Group to add from input
+   */
+  GroupToAdd: string;
+
+  /**
+   * Date to add from input
+   */
   pickedDate: string;
+
+  /**
+   * load tasks
+   */
 
   constructor() {
     this.loadTasks();
+    this.loadgroups();
   }
+
 
   /**
    * Load tasks from localStorage.
@@ -39,11 +70,34 @@ export class AppComponent {
     }
   }
 
+
+  /**
+   * Load groups from localStorage.
+   */
+  loadgroups() {
+    // Check localStorage for tasks
+    if (localStorage.getItem('groups ')) {
+      // Tasks are saved in localStorage, loop and add to tasks
+      for (const item of JSON.parse(localStorage.getItem('groups '))) {
+        // Add each task object as Task class
+        this.groups.push(new Group(item.name));
+
+      }
+    }
+  }
+
   /**
    * Save tasks to localStorage
    */
   saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  /**
+   * Save groups to localStorage
+   */
+  saveGroup() {
+    localStorage.setItem('groups', JSON.stringify(this.groups));
   }
 
   /**
@@ -53,6 +107,15 @@ export class AppComponent {
     this.tasks.unshift(new Task(this.taskToAdd, false, new Date(this.pickedDate), false));
     this.saveTasks();
     this.taskToAdd = '';
+  }
+
+  /**
+   * Add a group to task list and save to localStorage
+   */
+  addGroup() {
+    this.groups.unshift(new Group(this.GroupToAdd, []));
+    this.saveGroup();
+    this.GroupToAdd = '';
   }
 
   /**
@@ -70,7 +133,8 @@ export class AppComponent {
     // Save to localStorage
     this.saveTasks();
   }
-  pinTask(task: Task){
+
+  pinTask(task: Task) {
     task.pin = !task.pin;
     this.saveTasks();
   }
