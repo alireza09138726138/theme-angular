056c98e7b55d3abc9d75fba2d756c8463f23bc2f
Amir Savand <amir@savandbros.com>
2019-03-09 13:14:44 +0330

    Add Note.colors

--- src/app/models/note/note.ts
+++ src/app/models/note/note.ts
@@ -4,6 +4,20 @@
 export class Note {
 
   /**
+   * List of note colors
+   * Default color is the first item (white).
+   */
+  static colors: string[] = [
+    'white',
+    'info',
+    'danger',
+    'primary',
+    'warning',
+    'success',
+    'secondary',
+  ];
+
+  /**
    * Content of the note
    */
   content: string;
